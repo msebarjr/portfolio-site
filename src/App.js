b469e9f44c799b968a2439811744fef8cfdacaed
Michael Sebar Jr <msebarjr@gmail.com>
2022-04-26 13:04:10 -0400

    Updated Reset

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import "./App.css";
-
 function App() {
-<<<<<<< HEAD
     return <h1>Frontend Developer</h1>;
-=======
-    return <h1>Hello</h1>;
->>>>>>> 71389a903f5b0bed135f1a985b6eccc83e98a580
 }
 
 export default App;
